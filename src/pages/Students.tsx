48f8245f284279386fb64cb458b22030d3575aaf
kimyuna <dbskqnsghd@naver.com>
2023-04-26 16:11:32 +0900

    school administration

---
+++ src/pages/Students.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Students = () => {
+  return <div>students page</div>;
+};
+
+export default Students;
---
+++ src/redux/store.tsx
@@ -0,0 +1,7 @@
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+let store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
